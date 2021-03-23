ee429dcafbaa3475370cd61eb8d27e2b67f8cb44
Amber Hsiao <cs8430s1001215@gmail.com>
2021-03-23 22:14:44 +0800

    new version

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -14,3 +14,9 @@ sum_reviews = 0
 for d in data:
 	sum_reviews += len(d)
 print('平均留言長度為: ', sum_reviews/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
